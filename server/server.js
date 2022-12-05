085979055d1769f6ec826b3f94f7d16a2333e9f1
nrBaskar <106078300+nrBaskar@users.noreply.github.com>
2022-12-05 20:04:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8081;
 
 const server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
