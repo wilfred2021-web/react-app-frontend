b7b0e8719989597b3bf46aef6efef73f0c699592
wilfred2021-web <100276330+wilfred2021-web@users.noreply.github.com>
2022-12-16 12:58:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ const store = configureStore();
 
 
 render(
-    <Provider store={store}>
-        <App />
+    <Provider store={store}
+        <App /
     </Provider>,
     document.getElementById('root')
 );
