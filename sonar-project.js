af49e7285993d972d14c7a37ad11ed0bd13a8a93
wilfred2021-web <100276330+wilfred2021-web@users.noreply.github.com>
2023-06-26 11:44:28 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://3.110.210.24:9000',
+       serverUrl: 'http://13.1.0.4:9000',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'src/**' // Entry point of your code
